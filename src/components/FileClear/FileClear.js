7e3f08523e79b8d1225b7624e7e58d9b2cfff516
Tom Morris <thomasmorris823@gmail.com>
2018-05-04 17:10:19 +0100

    initial commit for clear file button

---
+++ src/components/FileClear/FileClear.js
@@ -0,0 +1,43 @@
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as actions from '../../actions/actions';
+import React from 'react';
+import './FileClear.css';
+
+class FileClear extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleFileClear = this.handleFileClear.bind(this);
+    }
+
+    handleFileClear(evt) {
+
+        alert('clearing file');
+    }
+
+
+    render() {
+        return (
+        <div className="fileClear" >
+            <button id="clear" name="clear" onClick={this.handleFileClear} className="clearButton">CLEAR</button>
+            <label for="clear">
+            <span>Clear File</span>
+            </label>
+        </div>
+        );
+    }
+}
+
+
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(actions, dispatch)
+    };
+}
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(FileClear);
