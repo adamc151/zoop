2230c0dcbb076baedc98d80b50ffd3dd1b27d5db
Adam Cooper <adamcooper1511@gmail.com>
2018-06-29 12:05:37 +0100

    delete icon

--- src/components/Accordian/AccordianItem.js
+++ src/components/Accordian/AccordianItem.js
@@ -6,11 +6,12 @@ export default function AccordianItem(props){
     const { dateString, description, location, amount } = props.transaction;
 
     return (
+
       <div className="accordianItemWrapper">
         <div className="itemDate">{dateString}</div>
         <div className="itemLocation">{location}</div>
         <div className="itemAmount">£{amount}</div>
-        <button className="itemButton" onClick={() => props.callback(props.index)}>X</button>
+        <div className="close-button" onClick={() => props.callback(props.index)}></div>
       </div>
     );
 
